9c42ba68621796b280abeb233e51adb11ea5acba
Kristina Ogorodova <skrok@inbox.ru>
2022-12-14 17:44:48 +0800

    added menu

--- js/menu.js
+++ js/menu.js
@@ -1,37 +1,37 @@
-import {overlay} from "./modal.js";
+import {overlay} from './modal.js';
 
 const menuOpenBtn = document.querySelector('.header__menu');
 const menu = document.querySelector('.mobile-menu');
 const logo = document.querySelector('.header__logo-link');
 const oderBtn = document.querySelector('.header__btn');
-const img =document.querySelector('.image-btn');
+const img = document.querySelector('.image-btn');
 
 const openMenu = () => {
-    menu.classList.add('active');
-    overlay.classList.add('overlay_open');
-    menuOpenBtn.classList.add('active');
-    logo.classList.add('active');
-    oderBtn.classList.add('active');
-    img.src = '../icons/close-menu-btn.png';
-}
+  menu.classList.add('active');
+  overlay.classList.add('overlay_open');
+  menuOpenBtn.classList.add('active');
+  logo.classList.add('active');
+  oderBtn.classList.add('active');
+  img.src = '../icons/close-menu-btn.png';
+};
 
 const closeMenu = () => {
-    menu.classList.remove('active');
-    overlay.classList.remove('overlay_open');
-    menuOpenBtn.classList.remove('active');
-    logo.classList.remove('active');
-    oderBtn.classList.remove('active');
-    img.src = '../img/menu.svg';
-}
+  menu.classList.remove('active');
+  overlay.classList.remove('overlay_open');
+  menuOpenBtn.classList.remove('active');
+  logo.classList.remove('active');
+  oderBtn.classList.remove('active');
+  img.src = '../img/menu.svg';
+};
 
 
 menuOpenBtn.addEventListener('click', openMenu);
 
 
 overlay.addEventListener('click', e => {
-    const target = e.target;
-    if (target === overlay || target.classList.closest('.header__menu')) {
-        closeMenu();
-    }
+  const target = e.target;
+  if (target === overlay || target.classList.closest('.header__menu')) {
+    closeMenu();
+  }
 });
 
--- js/modal.js
+++ js/modal.js
@@ -1,16 +1,15 @@
-const overlay = document.querySelector('.overlay');
-const modalOpenBtn =document.querySelector('.header__btn')
+export const overlay = document.querySelector('.overlay');
+const modalOpenBtn = document.querySelector('.header__btn');
 
 const openModal = () => overlay.classList.add('overlay_open');
 const closeModal = () => overlay.classList.remove('overlay_open');
 
-modalOpenBtn.addEventListener('click',openModal);
+modalOpenBtn.addEventListener('click', openModal);
 
 overlay.addEventListener('click', e => {
-    const target = e.target;
-    if (target === overlay || target.classList.contains('form-close-btn')) {
-        closeModal();
-    }
+  const target = e.target;
+  if (target === overlay || target.classList.contains('form-close-btn')) {
+    closeModal();
+  }
 });
 
-export {overlay,modalOpenBtn}
