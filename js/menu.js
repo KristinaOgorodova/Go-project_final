0ff74dcfb846d6d16d4c7516c9e1ffff95e9df95
Kristina Ogorodova <skrok@inbox.ru>
2022-12-19 19:52:16 +0800

    edit menu btn

--- js/menu.js
+++ js/menu.js
@@ -5,31 +5,36 @@ const logo = document.querySelector('.header__logo-link');
 const oderBtn = document.querySelector('.header__btn');
 const img = document.querySelector('.image-btn');
 const overlay = document.querySelector('.overlay');
-const headerContainer = document.querySelector('.header__container')
+const headerContainer = document.querySelector('.header__container');
+const menuCloseBtn = document.querySelector('.header__menu_close');
 
 const openMenu = () => {
   menu.classList.add('active');
   overlay.classList.add('overlay_open');
   headerContainer.classList.add('active');
-  img.src = '../icons/close-menu-btn.png';
+  menuCloseBtn.classList.add('active');
+  menuOpenBtn.classList.add('active')
+
 };
 
 const closeMenu = () => {
   menu.classList.remove('active');
   overlay.classList.remove('overlay_open');
-  menuOpenBtn.classList.remove('active');
+  menuOpenBtn.classList.toggle('active');
   logo.classList.remove('active');
   oderBtn.classList.remove('active');
-  img.src = '../img/menu.svg';
+  menuCloseBtn.classList.remove('active');
+  menuOpenBtn.classList.remove('active')
 };
 
 
 menuOpenBtn.addEventListener('click', openMenu);
+menuCloseBtn.addEventListener('click', closeMenu);
 
 
 overlay.addEventListener('click', e => {
   const target = e.target;
-  if (target === overlay || target.classList.closest('.header__menu')) {
+  if (target === overlay) {
     closeMenu();
   }
 });
